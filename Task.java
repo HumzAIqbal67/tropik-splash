c9dc86fdde069d27ea1b3505a38a776db1e850e5
Zaed Ul Islam <zaed.islam@mail.utoronto.ca>
2022-11-25 15:55:13 -0500

    Priority feature added to Tasks

--- Task.java
+++ Task.java
@@ -6,12 +6,14 @@ public class Task {
     public Date startDate;
     public Date dueDate;
     public int completionLength;
+    public int priority;
 
-    public Task(String taskName, Date startDate, Date dueDate, int completionLength) {
+    public Task(String taskName, Date startDate, Date dueDate, int completionLength, int priority) {
         this.taskName = taskName;
         this.startDate = startDate;
         this.dueDate = dueDate;
         this.completionLength = completionLength;
+        this.priority = priority;
     }
 
     public void changeName(String newName) {
@@ -21,12 +23,15 @@ public class Task {
         this.startDate = newStart;
     }
 
-
     public void changeDue(Date newDue) {
         this.dueDate = newDue;
     }
 
     public void changeLen(int newLen) {
         this.completionLength = newLen;
+    }
+
+    public void changePriority(int newPriority) {
+        this.priority = newPriority;
     }
 }
